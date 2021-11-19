1df4ad997294d0a4de4fb45b36f3501eab99b42f
BisheshworKhadka <94677563+BisheshworKhadka@users.noreply.github.com>
2021-11-19 14:24:35 +0545

    first commit

---
+++ Bisheshwor Khadka/index.js
@@ -0,0 +1,12 @@
+
+
+function display(){
+    let n = document.getElementById('name').value;
+    let roll = document.getElementById('roll').value;
+    
+    let clas = document.getElementById('class').value;
+    
+    console.log("name is: ",n );
+    console.log("class is: " )
+
+}
